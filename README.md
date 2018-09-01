4311ad92d7dfba157403be71affce9fc025506ae
damnee562 <damnee562@gmail.com>
2018-09-01 16:52:20 +0800

    Prepare setuptools

---
+++ README.md
@@ -0,0 +1,5 @@
+# Redash-API-Client
+Redash API Client written in Python.
+
+## License
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="redash-api-client",
+    version="0.0.1",
+    author="Damien Zeng",
+    author_email="damnee562@gmail.com",
+    description="Redash API Client",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/damnee562/redash-api-client",
+    packages=setuptools.find_packages(),
+    install_requires=['requests'],
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
