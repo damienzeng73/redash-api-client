4bea9dd2f725cc704151ac01fb08bbaaa66433a2
Damien Zeng <damnee562@gmail.com>
2019-10-01 13:10:15 +0800

    Publish new version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="redash-api-client",
-    version="0.1.4",
+    version="0.1.5",
     author="Damien Zeng",
     author_email="damnee562@gmail.com",
     description="Redash API Client",
