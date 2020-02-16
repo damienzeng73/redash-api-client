6018b85cdcdfd09b8511779393b82266f65ebac8
Damien Zeng <damnee562@gmail.com>
2020-02-16 16:45:22 +0800

    Fix widget related functions

--- redashAPI/client.py
+++ redashAPI/client.py
@@ -2,7 +2,7 @@ import json
 import requests
 
 class RedashAPIClient:
-    def __init__(self, api_key: str, host: str='http://localhost:5000'):
+    def __init__(self, api_key: str, host: str="http://localhost:5000"):
         self.api_key = api_key
         self.host = host
 
@@ -206,13 +206,8 @@ class RedashAPIClient:
         return self.post('dashboards', payload)
 
     def add_widget(self, db_id: int, text: str="", vs_id: int=None, full_width: bool=False, position: dict=None):
-        res = self.get(f"dashboards")
-
-        results = res.json().get('results', [])
-        slug = next((d['slug'] for d in results if d['id'] == db_id), None)
-
-        if slug is None:
-            raise Exception("Dashboard not found.")
+        if position is None or not isinstance(position, dict):
+            position = {}
 
         payload = {
             "dashboard_id": db_id,
@@ -220,27 +215,31 @@ class RedashAPIClient:
             "visualization_id": vs_id,
             "width": 1,
             "options": {
-                "position": position or self.calculate_widget_position(slug, full_width)
+                "position": position or self.calculate_widget_position(db_id, full_width)
             }
         }
 
         return self.post('widgets', payload)
 
-    def calculate_widget_position(self, slug: str, full_width: bool):
+    def calculate_widget_position(self, db_id: int, full_width: bool):
+        res = self.get('dashboards')
+        dashboards = res.json().get('results', [])
+        slug = next((d['slug'] == db_id for d in dashboards if d['id'] == db_id), None)
+
+        res = self.get(f'dashboards/{slug}')
+        widgets = res.json().get('widgets', [])
+
+        full_width_widgets_count = 0
+        for w in widgets:
+            if w['options']['position']['sizeX'] == 6:
+                full_width_widgets_count += 1
+
         position = {
             "col": 0,
             "row": 0,
             "sizeX": 3,
             "sizeY": 8
         }
-
-        res = self.get(f'dashboards/{slug}')
-        widgets = res.json().get('widgets', None)
-
-        full_width_widgets_count = 0
-        for w in widgets:
-            if w['options']['position']['sizeX'] == 6:
-                full_width_widgets_count += 1
 
         if len(widgets) > 0:
             row, col = divmod(len(widgets) - full_width_widgets_count, 2)
@@ -254,10 +253,7 @@ class RedashAPIClient:
         return position
 
     def publish_dashboard(self, db_id: int):
-        try:
-            self.post(f"dashboards/{db_id}", {"is_draft": False})
-        except Exception as e:
-            raise e
+        self.post(f"dashboards/{db_id}", {"is_draft": False})
 
         res = self.post(f"dashboards/{db_id}/share")
         public_url = res.json().get('public_url', None)
